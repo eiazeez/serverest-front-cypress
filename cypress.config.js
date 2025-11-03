c585f835f955dabdc7ac16f9ee2b24c318e787a4
Isaac Douglas <isaac.douglas08@gmail.com>
2025-11-03 17:32:34 -0300

    :wrench: Config: Ajustando resolução para 1600x900

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 900,
+    viewportWidth: 1600,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
