a5016210e999b50ef2c246ef7b06f83e1ef72b88
Isaac Douglas <isaac.douglas08@gmail.com>
2025-11-25 16:36:44 -0300

    :wrench: Config: Ajustando Timeout de Load Page e Command

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     baseUrl: "https://front.serverest.dev",
     viewportHeight: 900,
     viewportWidth: 1600,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
